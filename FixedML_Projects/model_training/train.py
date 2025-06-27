8286e25424ae48c193c13e811e7d923413b0815a
ShirouSharma <99466435+ShirouSharma@users.noreply.github.com>
2025-06-27 11:00:46 +0530

    train.py

--- FixedML_Projects/model_training/train.py
+++ FixedML_Projects/model_training/train.py
@@ -15,7 +15,7 @@ X = data.drop('median_house_value', axis=1)
 y = data['median_house_value']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor(n_estimators=50, random_state=42)  # Reduced size
+model = RandomForestRegressor(n_estimators=50, random_state=42)  
 model.fit(X_train, y_train)
 
 # Save model to API folder
